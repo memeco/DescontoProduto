362d93707eed3adca4fb0826dbd7a99e6efe6cfa
Emerson Marques Pedro <emersonmp@gmail.com>
2024-03-09 13:30:37 -0300

    Classe e Util inclusos

---
+++ DescontoProduto/DescontoProduto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DescontoProduto
+{
+    public class DescontoProduto
+    {
+        private string nome;
+        private double preco;
+        private int quantidade;
+
+        public DescontoProduto(string nome, double preco, int quantidade)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        public double CalcularValorTotalSemDesconto()
+        {
+            return quantidade * preco;
+        }
+
+        public double CalcularDesconto()
+        {
+            if (quantidade >= 11 && quantidade <= 20)
+            {
+                return 0.1;
+            }
+            else if (quantidade >= 21 && quantidade <= 50)
+            {
+                return 0.2;
+            }
+            else if (quantidade > 50)
+            {
+                return 0.25;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double CalcularValorTotalComDesconto()
+        {
+            double valorTotalSemDesconto = CalcularValorTotalSemDesconto();
+            double desconto = CalcularDesconto();
+            return valorTotalSemDesconto - (valorTotalSemDesconto * desconto);
+        }
+
+        public string ObterNome()
+        {
+            return nome;
+        }
+
+        public double ObterPreco()
+        {
+            return preco;
+        }
+
+        public int ObterQuantidade()
+        {
+            return quantidade;
+        }
+    }
+}
--- DescontoProduto/Program.cs
+++ DescontoProduto/Program.cs
@@ -12,62 +12,22 @@ namespace DescontoProduto
 
             // Leitura do preço do produto
             Console.WriteLine("Digite o preço do produto: ");
-            double precoProduto = LerDouble();
+            double precoProduto = Util.LerValorDouble(); // Use Util.LerValorDouble()
 
             // Leitura da quantidade do produto
             Console.WriteLine("Digite a quantidade do produto: ");
-            int quantidadeProduto = LerInt();
+            int quantidadeProduto = Util.LerValorInteiro(); // Use Util.LerValorInteiro()
 
-            // Cálculo do valor total sem desconto
-            double valorTotalSemDesconto = quantidadeProduto * precoProduto;
-
-            // Cálculo do desconto
-            double desconto = 0;
-            if (quantidadeProduto >= 11 && quantidadeProduto <= 20)
-            {
-                desconto = 0.1;
-            }
-            else if (quantidadeProduto >= 21 && quantidadeProduto <= 50)
-            {
-                desconto = 0.2;
-            }
-            else if (quantidadeProduto > 50)
-            {
-                desconto = 0.25;
-            }
-
-            // Cálculo do valor total com desconto
-            double valorTotalComDesconto = valorTotalSemDesconto - (valorTotalSemDesconto * desconto);
+            // Criação da instância da classe DescontoProduto
+            DescontoProduto descontoProduto = new DescontoProduto(nomeProduto, precoProduto, quantidadeProduto);
 
             // Exibição dos resultados
-            Console.WriteLine("Nome do produto: {0}", nomeProduto);
-            Console.WriteLine("Preço do produto: R${0}", precoProduto.ToString("F2"));
-            Console.WriteLine("Quantidade do produto: {0}", quantidadeProduto);
-            Console.WriteLine("Valor total sem desconto: R${0}", valorTotalSemDesconto.ToString("F2"));
-            Console.WriteLine("Desconto: {0}%", (desconto * 100).ToString("F2"));
-            Console.WriteLine("Valor total com desconto: R${0}", valorTotalComDesconto.ToString("F2"));
-        }
-
-        // Função para ler um valor double do console
-        static double LerDouble()
-        {
-            double valor;
-            while (!double.TryParse(Console.ReadLine(), out valor))
-            {
-                Console.WriteLine("Valor inválido. Digite novamente: ");
-            }
-            return valor;
-        }
-
-        // Função para ler um valor int do console
-        static int LerInt()
-        {
-            int valor;
-            while (!int.TryParse(Console.ReadLine(), out valor))
-            {
-                Console.WriteLine("Valor inválido. Digite novamente: ");
-            }
-            return valor;
+            Console.WriteLine("Nome do produto: {0}", descontoProduto.ObterNome());
+            Console.WriteLine("Preço do produto: R${0}", descontoProduto.ObterPreco().ToString("F2"));
+            Console.WriteLine("Quantidade do produto: {0}", descontoProduto.ObterQuantidade());
+            Console.WriteLine("Valor total sem desconto: R${0}", descontoProduto.CalcularValorTotalSemDesconto().ToString("F2"));
+            Console.WriteLine("Desconto: {0}%", (descontoProduto.CalcularDesconto() * 100).ToString("F2"));
+            Console.WriteLine("Valor total com desconto: R${0}", descontoProduto.CalcularValorTotalComDesconto().ToString("F2"));
         }
     }
 }
